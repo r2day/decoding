4e34a0e0040af3b3e09f89050c695908b049b0e6
dropthemasquerade <dropthemasquerade@gmail.com>
2023-01-05 22:05:58 +0800

    fixed Illegal base64 data at input byte 4 when using base64.StdEncoding.DecodeString(str)

--- base64tofile.go
+++ base64tofile.go
@@ -3,17 +3,20 @@ package decoding
 import (
 	"encoding/base64"
 	"os"
+	"strings"
 )
 
 
 func Base64ToFile(b64 string, f *os.File) error {
+	var dec []byte
+	var err error
 
+	b64data := b64[strings.IndexByte(b64, ',')+1:]
 	// 解码
-	dec, err := base64.StdEncoding.DecodeString(b64)
-    if err != nil {
-        return err
-    }
-
+	dec, err = base64.StdEncoding.DecodeString(b64data)
+	if err != nil {
+		return err
+	}
 	// 写入流文件
     if _, err := f.Write(dec); err != nil {
         return err
